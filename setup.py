527e3e1cdd82ffd1ab32691c01eb7b49a4abee2c
lollo03 <03lollo@gmail.com>
2023-03-23 21:54:34 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='termplots',
+    packages=find_packages(include=['termplots']),
+    version='0.1.0',
+    description='A lightweight plot library for your terminal!',
+    author='Lorenzo Andreasi',
+    license='MIT',
+    install_requires=[],
+)
---
+++ test.py
@@ -0,0 +1,4 @@
+import termplots as tmp
+
+tmp.mplot([[1,0,2], [-1,0,3,0,2,2]], car=['*', '$', '@'], labels=['List 1', 'List 2'])
+
